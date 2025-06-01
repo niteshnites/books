d9f23bb9daf44735ac9bcdab0c0443ddcc834ee0
niteshnites <niteshnites3@gmail.com>
2025-06-01 19:16:42 +0530

    global exception handling

--- src/main/java/com/luv2code/books/controller/BookController.java
+++ src/main/java/com/luv2code/books/controller/BookController.java
@@ -120,6 +120,16 @@ public class BookController {
         return new ResponseEntity<>(bookErrorResponse, HttpStatus.NOT_FOUND);
     }
 
+    @ExceptionHandler
+    public ResponseEntity<BookErrorResponse> handleException(Exception exc) {
+        BookErrorResponse bookErrorResponse = new BookErrorResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                "Invalid request!",
+                System.currentTimeMillis()
+        );
+        return new ResponseEntity<>(bookErrorResponse, HttpStatus.BAD_REQUEST);
+    }
+
 }
 
 
